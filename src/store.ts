db407a0101c1b3051408d259ebc925203732e328
Tyler Liu <tyler4long@gmail.com>
2024-11-12 11:25:27 -0800

    Create store

--- src/store.ts
+++ src/store.ts
@@ -1,7 +1,27 @@
+import hyperid from 'hyperid';
 import { manage } from 'manate';
 
+const uuid = hyperid();
+
+export class Todo {
+  public id = uuid();
+  public text: string;
+  public done = false;
+}
+
 export class Store {
-  public count = 0;
+  public todos: Todo[] = [];
+  public filter: 'all' | 'complete' | 'incomplete' = 'all';
+  public get filteredTodos() {
+    switch (this.filter) {
+      case 'all':
+        return this.todos;
+      case 'complete':
+        return this.todos.filter((todo) => todo.done);
+      case 'incomplete':
+        return this.todos.filter((todo) => !todo.done);
+    }
+  }
 }
 
 const store = manage(new Store());
